4cbd833b40befe84231792eb9fcf3ee9b80bc44d
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-14 12:13:12 -0300

    Styling JokeList Component

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./JokeList.css";
 
 class JokeList extends Component {
   static defaultProps = {
@@ -23,7 +24,13 @@ class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Dad Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(joke => (
             <div>{joke}</div>
